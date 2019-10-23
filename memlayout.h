04a807561c54c264f18de4c81bf35d0ce389d6d7
wangyuntao <wyt.daily@gmail.com>
2019-10-23 23:08:13 +0800

    A test commit

--- memlayout.h
+++ memlayout.h
@@ -4,6 +4,8 @@
 #define PHYSTOP 0xE000000           // Top physical memory
 #define DEVSPACE 0xFE000000         // Other devices are at high addresses
 
+// test2
+
 // Key addresses for address space layout (see kmap in vm.c for layout)
 #define KERNBASE 0x80000000         // First kernel virtual address
 #define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
@@ -13,3 +15,5 @@
 
 #define V2P_WO(x) ((x) - KERNBASE)    // same as V2P, but without casts
 #define P2V_WO(x) ((x) + KERNBASE)    // same as P2V, but without casts
+
+// test
